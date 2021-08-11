a734ef8428f29132bf72b7fc490904bc0d12ee4a
kulwinder_gill <kulwinder.singh.goteso@gmail.com>
2021-08-11 18:30:42 +0530

    day1

---
+++ database/seeds/ProductSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert([
+            'product_name' => Str::random(10),
+
+        ]);
+    }
+}
